bf77ca6e55a1ffa3b3b4aeaad5c10bce81e83cd1
PauloCesar08 <ppns2006@gmail.com>
2025-07-06 21:32:12 -0300

    conclusão do HTML e CSS

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/*  setTimeout--> executa uma vez
+    setInterval--> executa infinitas vezes
+ */
+
+   setTimeout(function() {
+        alert("executed")
+    }, 3000); //1000 milisegundos = 1 segundo
